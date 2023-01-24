ef84a1ddabd10748c8397b03060e343605495da2
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-01-24 22:48:56 +0100

    Finish tailwind setup and add login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,38 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 
 const Login = () => {
   const navigate = useNavigate();
-
   const { firebase } = useContext(FirebaseContext);
 
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - NIT-SOCIAL";
+  }, []);
+
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="Iphone profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="flex justify-center w-full">
+          <img
+            src="/images/logo.png"
+            alt="nit-social"
+            className="mt-2 w-6/12 mb-4"
+          />
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        </h1>
+      </div>
     </div>
   );
 };
